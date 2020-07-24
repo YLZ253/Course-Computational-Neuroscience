08fecb860a8bfb09bee5facb118f141903697004
YLZ253 <65844614+YLZ253@users.noreply.github.com>
2020-07-24 21:42:20 +0800

    Update spike-triggered-average.m

--- spike-triggered-average/spike-triggered-average.m
+++ spike-triggered-average/spike-triggered-average.m
@@ -1,1 +1,29 @@
-o
+close all; clear all; clc;
+
+load('c1p8.mat');
+
+
+
+sampling_period =2 ; % sampling rate is 500 Hz
+
+num_timesteps =150 ; % window width
+
+
+
+sta = compute_sta(stim, rho, num_timesteps);
+
+
+
+time = -sampling_period*(num_timesteps-1):sampling_period:0; % in ms
+
+
+
+figure(1);
+
+plot(time, sta);
+
+xlabel('Time (ms)');
+
+ylabel('Stimulus');
+
+title('Spike-Triggered Average');
